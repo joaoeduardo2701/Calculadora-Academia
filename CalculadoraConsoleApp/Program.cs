56752c8c775dfd94d917d5c6600a1b8e819cd4f8
João Eduardo Cardoso Padilha <duducardoso2701@gmail.com>
2024-03-13 15:05:09 -0300

    retirando multiplicação e divisão da aula 02

--- CalculadoraConsoleApp/Program.cs
+++ CalculadoraConsoleApp/Program.cs
@@ -14,20 +14,18 @@ namespace calculadora
                 Console.WriteLine();
                 Console.WriteLine("[ 1 ] Somar");
                 Console.WriteLine("[ 2 ] Subtrair");
-                Console.WriteLine("[ 3 ] Multiplicar");
-                Console.WriteLine("[ 4 ] Dividir");
-                Console.WriteLine("[ 5 ] Sair do sistema");
+                Console.WriteLine("[ 3 ] Sair do sistema");
                 Console.Write("->");
 
                 int operacao = int.Parse(Console.ReadLine());
 
-                if (operacao == 5)
+                if (operacao == 3)
                 {
                     Console.WriteLine("Encerrando programa...");
                     break;
                 } 
 
-                if (operacao > 5 || operacao < 1)
+                if (operacao > 3 || operacao < 1)
                 {
                     Console.WriteLine("Por favor digite uma opção válida!!!");
                     Console.WriteLine("Digite qualquer tecla para continuar... ");
@@ -50,22 +48,8 @@ namespace calculadora
                 } else if (operacao == 2) 
                 {
                     resultado = primeironumero - segundonumero;
-                } else if (operacao == 3) 
-                {
-                    resultado = primeironumero * segundonumero;
-                } else if(operacao == 4)
-                {   
-                    if (segundonumero == 0)
-                    {
-                        Console.WriteLine("Não é possível dividir um número por zero");
-                        Console.WriteLine("Digite qualquer tecla para continuar...");
-                        Console.ReadKey();
-                        continue;
-                    } else
-                    {
-                        resultado = primeironumero / segundonumero;
-                    }
-                } else
+                } 
+                else
                 {
                     Console.WriteLine("Por favor digite uma opção válida!!! ");
                 }
