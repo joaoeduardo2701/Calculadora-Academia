7dc2adc00382c110f698028462760dd6ed44610a
João Eduardo Cardoso Padilha <duducardoso2701@gmail.com>
2024-03-19 16:53:33 -0300

    Código refatorado

--- CalculadoraConsoleApp/Program.cs
+++ CalculadoraConsoleApp/Program.cs
@@ -1,80 +1,118 @@
 ﻿using System.Globalization;
 
-namespace calculadora
+namespace calculadora;
+
+internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        while (true)
         {
-            while (true) 
+            int operacao = MostrarMenu();
+
+            if (OpcaoSaidaSelecionada(operacao))
             {
-                Console.Clear();
-
-                Console.WriteLine("-- Calculadora Tabajara 2024 --");
-                Console.WriteLine();
-                Console.WriteLine("[ 1 ] Somar");
-                Console.WriteLine("[ 2 ] Subtrair");
-                Console.WriteLine("[ 3 ] Multiplicar");
-                Console.WriteLine("[ 4 ] Dividir");
-                Console.WriteLine("[ 5 ] Sair do sistema");
-                Console.Write("->");
-
-                int operacao = int.Parse(Console.ReadLine());
-
-                if (operacao == 5)
-                {
-                    Console.WriteLine("Encerrando programa...");
-                    break;
-                } 
-
-                if (operacao > 5 || operacao < 1)
-                {
-                    Console.WriteLine("Por favor digite uma opção válida!!!");
-                    Console.WriteLine("Digite qualquer tecla para continuar... ");
-                    Console.ReadKey();
-                    continue;
-                }
-
-                Console.WriteLine("Digite o primeiro numero: ");
-                double primeironumero = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-
-                Console.WriteLine("Digite o segundo numero: ");
-                double segundonumero = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-
-                double resultado = 0;
-
-                if (operacao == 1)
-                {
-                    resultado = primeironumero + segundonumero;
-
-                } else if (operacao == 2) 
-                {
-                    resultado = primeironumero - segundonumero;
-                } else if (operacao == 3) 
-                {
-                    resultado = primeironumero * segundonumero;
-                } else if(operacao == 4)
-                {   
-                    if (segundonumero == 0)
-                    {
-                        Console.WriteLine("Não é possível dividir um número por zero");
-                        Console.WriteLine("Digite qualquer tecla para continuar...");
-                        Console.ReadKey();
-                        continue;
-                    } else
-                    {
-                        resultado = primeironumero / segundonumero;
-                    }
-                } else
-                {
-                    Console.WriteLine("Por favor digite uma opção válida!!! ");
-                }
-           
-                Console.WriteLine("O resultado é " + resultado.ToString("F2", CultureInfo.InvariantCulture));
-
-                Console.WriteLine("Digite qualquer tecla para continuar...");
-                Console.ReadKey();
+                break;
+            } else if (OpcaoValida(operacao))
+            {
+                MensagemDeErro();
+                continue;
+            } else
+            {
+                RealizarCalculo(operacao);
             }
         }
+    }
+
+
+    static int MostrarMenu()
+    {
+        Console.Clear();
+
+        Console.WriteLine("-- Calculadora Tabajara 2024 --");
+        Console.WriteLine();
+        Console.WriteLine("[ 1 ] Somar");
+        Console.WriteLine("[ 2 ] Subtrair");
+        Console.WriteLine("[ 3 ] Multiplicar");
+        Console.WriteLine("[ 4 ] Dividir");
+        Console.WriteLine("[ 5 ] Sair do sistema");
+        Console.Write("->");
+
+        int operacao = int.Parse(Console.ReadLine());
+
+        return operacao;
+    }
+
+    static bool OpcaoSaidaSelecionada(int operacao)
+    {
+        bool opcaoSaidaSelecionada = operacao == 5;
+         
+        return opcaoSaidaSelecionada;
+    }
+
+    static bool OpcaoValida(int operacao)
+    {
+        bool opcaoValida = operacao > 5 || operacao < 1;
+
+        return opcaoValida;
+    }
+    static void MensagemDeErro()
+    {
+        Console.WriteLine("Por favor digite uma opção válida!!!");
+        Console.WriteLine("Digite qualquer tecla para continuar... ");
+        Console.ReadKey();
+    }
+    private static double ObterValorNumerico(string texto)
+    {
+        Console.WriteLine(texto);
+        return double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+    }
+    private static void RealizarCalculo(int operacao)
+    {
+        double primeironumero = ObterValorNumerico("Digite o primeiro número: ");
+        double segundonumero = ObterValorNumerico("Digite o segundo numero: ");
+
+        double resultado = 0;
+
+        switch (operacao)
+        {
+            case 1:
+                resultado = primeironumero + segundonumero;
+                break;
+            case 2:
+                resultado = primeironumero - segundonumero;
+                break;
+            case 3:
+                resultado = primeironumero * segundonumero;
+                break;
+            case 4:
+                if (segundonumero == 0)
+                {
+                    Console.WriteLine("Não é possível dividir um número por zero");
+                    Console.WriteLine("Digite qualquer tecla para continuar...");
+                    Console.ReadKey();
+                    //continue;
+                }
+                else
+                {
+                    resultado = primeironumero / segundonumero;
+                }
+                break;
+            default:
+                Console.WriteLine("Por favor digite uma opção válida!!! ");
+                break;
+        }
+
+        MostrarResultados(resultado);
+
+
+    }
+
+    static void MostrarResultados(double resultado)
+    {
+        Console.WriteLine("O resultado é " + resultado.ToString("F2", CultureInfo.InvariantCulture));
+
+        Console.WriteLine("Digite qualquer tecla para continuar...");
+        Console.ReadKey();
     }
 }
